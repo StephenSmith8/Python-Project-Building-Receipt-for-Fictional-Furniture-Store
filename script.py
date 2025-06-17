6ca319c2e3864b3c41a898bb5f287ed28de6b766
Stephen Smith <112740787+StephenSmith8@users.noreply.github.com>
2025-06-17 19:04:41 -0400

    Create script.py

---
+++ script.py
@@ -0,0 +1,52 @@
+#Defining the lovely loveseat description variable
+
+lovely_loveseat_variable = "Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white."
+
+#Defining a price variable for the loveseat
+
+lovely_loveseat_price = 254.00
+
+#Defining description for furniture piece "Stylish Sette"
+
+stylish_settee_description = "Stylish Settee. Faux leather on birch. 29.50 inches high x 54.75 inches wide x 28 inches deep. Black."
+
+#Defining price for stylish sette
+
+stylish_settee_price = 180.50
+
+#Defining description for luxurious lamp
+
+luxurious_lamp_description = "Luxurious Lamp. Glass and iron. 36 inches tall. Brown with cream shade."
+#Defining price for luxurious lamp
+
+luxurious_lamp_price = 52.15 
+
+#Defining Sales Tax
+
+sales_tax = 0.088
+
+#Defining total for first customer
+
+customer_one_total = 306.15
+
+#Defining first customer's itemized purchase list
+
+customer_one_itemization = "lovely_loveseat_variable, luxurious_lamp_description"
+
+#Defining sales tax calculation for the first customer
+
+customer_one_tax = customer_one_total * sales_tax
+
+print ("Customer One Items:")
+
+print (customer_one_itemization)
+
+print ("Customer One Total:")
+
+print (customer_one_total)
+
+
+
+
+
+
